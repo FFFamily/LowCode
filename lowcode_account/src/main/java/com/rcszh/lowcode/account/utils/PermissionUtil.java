b878566afe02543230c20fe2e9320b70ca322560
tujunjie <747487547@qq.com>
2024-08-19 18:12:50 +0800

    feat：角色权限

--- lowcode_account/src/main/java/com/rcszh/lowcode/account/utils/PermissionUtil.java
+++ lowcode_account/src/main/java/com/rcszh/lowcode/account/utils/PermissionUtil.java
@@ -9,11 +9,5 @@ import java.util.List;
  * 解析权限工具类
  */
 public class PermissionUtil {
-    /**
-     * 获取角色权限列表
-     */
-    public List<String> getRolePermission(Role role) {
-        List<String> permissions = new ArrayList<String>();
-        if ()
-    }
+
 }
