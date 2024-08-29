8d359a8c9ce31179395b152bcb23dbac4e18e8a3
tujunjie <747487547@qq.com>
2024-08-29 18:08:55 +0800

    feat：补充SQL

--- sql/init.sql
+++ sql/init.sql
@@ -22,6 +22,33 @@ create table core_data_source_table
         primary key,
     code_data_source_id int          null,
     name                varchar(255) null,
-    type                int          null,
-    table_name          varchar(255) null
+    type                varchar(50)  null,
+    table_name          varchar(255) null,
+    tenant_id           int          null,
+    create_by           varchar(255) null,
+    create_time         datetime     null,
+    update_by           varchar(255) null,
+    update_time         datetime     null,
+    remark              varchar(255) null
 );
+
+create table core_data_source_field
+(
+    id            int auto_increment primary key ,
+    table_id      int          null,
+    type          int          null,
+    name          varchar(255) null,
+    field_name    varchar(255) null,
+    field_type    varchar(255) null,
+    field_comment varchar(255) null,
+    field_default varchar(255) null,
+    field_index   varchar(255) null,
+    tenant_id     int          null,
+    create_by     int          null,
+    create_time   datetime     null,
+    update_by     int          null,
+    update_time   datetime     null
+);
+
+
+
