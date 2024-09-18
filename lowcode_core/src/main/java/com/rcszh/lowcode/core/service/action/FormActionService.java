7a04b99d439a355d4b73728ab3a1dc23aa8f133c
FFFamily <49579222+FFFamily@users.noreply.github.com>
2024-09-19 07:51:21 +0800

    feat: 修改方法

--- lowcode_core/src/main/java/com/rcszh/lowcode/core/service/action/FormActionService.java
+++ lowcode_core/src/main/java/com/rcszh/lowcode/core/service/action/FormActionService.java
@@ -8,6 +8,7 @@ import com.rcszh.lowcode.core.mapper.action.FormActionMapper;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -21,19 +22,27 @@ public class FormActionService {
         formActionMapper.insert(formAction);
     }
 
-    public HashMap<String, FormAction> findActionByConditionToMap(List<String> formConditionList) {
+    /**
+     *
+     * @param formConditionList
+     * @return Map{key:formConditionId,value:Action}
+     */
+    public HashMap<String, List<FormAction>> findActionByConditionToMap(List<String> formConditionList) {
         // 中间表数据
         List<FormActionCondition> formActionConditions = formActionConditionMapper.selectList(new LambdaQueryWrapper<FormActionCondition>()
                 .in(FormActionCondition::getConditionId, formConditionList));
         HashMap<String, String> mapping = formActionConditions.stream().reduce(new HashMap<String, String>(), (prev, curr) -> {
-            prev.put(curr.getConditionId(), curr.getActionId());
+            prev.put(curr.getActionId(), curr.getConditionId());
             return prev;
         }, (l, r) -> l);
         // 中间表对应的action配置
         return formActionMapper.selectList(new LambdaQueryWrapper<FormAction>()
                         .in(FormAction::getId, formActionConditions.stream().map(FormActionCondition::getActionId).toList()))
-                .stream().reduce(new HashMap<String, FormAction>(), (prev, curr) -> {
-                    prev.put(curr.getId(), curr);
+                .stream().reduce(new HashMap<String, List<FormAction>>(), (prev, curr) -> {
+                    String formConditionId = mapping.get(curr.getId());
+                    List<FormAction> list = prev.getOrDefault(formConditionId, new ArrayList<>());
+                    list.add(curr);
+                    prev.put(formConditionId, list);
                     return prev;
                 }, (l, r) -> l);
     }
